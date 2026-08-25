806648c7b46fc219a8b9fa3d3e9d5617dca4cd99
jordanandrevon <jordan.andrevon@gmail.com>
2026-08-25 14:59:09 +0200

    Modification du app.py pour afficher le csv taxi

--- app.py
+++ app.py
@@ -4,3 +4,31 @@ import pandas as pd
 st.title("Mon Premier Dashboard Streamlit")
 # Texte de présentation
 st.write("Bienvenue sur cette application interactive dédiée à l'analyse de données.")
+import streamlit as st
+import pandas as pd
+
+# Chargement du fichier CSV
+df = pd.read_csv("taxis.csv")
+
+# Titre personnalisé
+st.title("Dashboard Analyse Taxis - Jordan")
+
+# Menu déroulant pour choisir un arrondissement
+boroughs = df["pickup_borough"].dropna().unique()
+
+borough = st.selectbox(
+    "Choisissez un quartier de prise en charge :",
+    boroughs
+)
+
+# Filtrer le dataframe selon l'arrondissement choisi
+df_filtre = df[df["pickup_borough"] == borough]
+
+# Afficher les 5 premières lignes
+st.dataframe(df_filtre.head())
+
+# Afficher le nombre total de courses
+st.metric(
+    "Nombre total de courses",
+    len(df_filtre)
+)
